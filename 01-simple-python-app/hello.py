a51335a7851f35c6eee9e9308d4c9e65ea639077
markferguson <mark.ferguson0@gmail.com>
2017-07-25 20:56:56 +0100

    Updated file route

--- 01-simple-python-app/hello.py
+++ 01-simple-python-app/hello.py
@@ -1,9 +1,6 @@
 """
 A first simple Cloud Foundry Flask app
 
-Author: Ian Huston
-License: See LICENSE.txt
-
 """
 from flask import Flask
 import os
@@ -12,10 +9,24 @@ app = Flask(__name__)
 
 # Get port from environment variable or choose 9099 as local default
 port = int(os.getenv("PORT", 9099))
+home = str(os.getenv('HOME'))
+env = str(os.getenv('APP_ENV', 'unknown'))
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
+def hello_world():    
+    msg = ' *** Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
+    return home + ' - ' + env + ' - ' + msg
+
+@app.route('/list')
+def listing():
+    return str(os.listdir(home))
+
+@app.route('/file')
+def file_processing():
+    myfile = open(os.getenv('FILE_LOCATION', home) + '/readme.md')
+    sf = str(myfile.name)
+    print sf
+    return sf
 
 if __name__ == '__main__':
     # Run the app, listening on all IPs with our chosen port number
